df5a29d6039ae7f78782e703dd31050ad98c54d0
Jaemin Song <139603039+jaemin-song876@users.noreply.github.com>
2025-06-17 00:12:37 -0500

    Time: 19 ms (44.49%), Space: 12.6 MB (99.15%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,12 +5,15 @@ class Solution(object):
         :rtype: bool
         """
         left, right=0, len(s) -1
+        #문자 양끝에서 포인터 시작 
         while left<right:
+            # 왼쪽 문자 스킵
             while left< right and not s[left].isalnum():
                 left += 1
+            #오른쪽 문자 스킵
             while left<right and not s[right].isalnum():
                 right -=1
-
+            # 소문자로 바꿔서 비교
             if s[left].lower() != s[right].lower():
                 return False
             
