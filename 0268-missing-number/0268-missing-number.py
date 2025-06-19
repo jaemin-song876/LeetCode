90cfb0a08bcc83c3e6fdb9e2e024f1ee580eae15
Jaemin Song <139603039+jaemin-song876@users.noreply.github.com>
2025-06-19 17:37:08 -0500

    Time: 7 ms (46.36%), Space: 13.4 MB (30.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -3,8 +3,20 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
-        """
+        
         n = len(nums)
         for i in range(n+1):
             if i not in nums:
-                return i
+                return i"""
+
+        n = len(nums)
+        xor_all = 0  #0~n까지의숫자전체를 xor할 변수
+        xor_nums = 0 # nums 배열에 있는 숫자들을 xor할 변수
+        for i in range(n+1):
+            xor_all ^= i
+
+        for num in nums:
+            xor_nums ^= num
+
+        return xor_all ^ xor_nums
+
