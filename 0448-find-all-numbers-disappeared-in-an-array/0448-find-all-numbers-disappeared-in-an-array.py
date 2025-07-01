8306f796eb440db66ce6582633fe49e0ee262d60
Jaemin Song <139603039+jaemin-song876@users.noreply.github.com>
2025-07-01 10:39:22 -0500

    Time: 77 ms (8.55%), Space: 29.1 MB (35.32%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -3,11 +3,25 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: List[int]
-        """
+        
 
         set_nums = set(nums)
         result =[]
         for i in range(1,len(nums)+1):
             if i not in set_nums:
                 result.append(i)
+        return result
+        """
+        i=0
+        result =[]
+
+        while i < len(nums):
+            while nums[i] != nums[nums[i] -1]:
+                nums[nums[i] -1], nums[i] = nums[i], nums[nums[i]-1]
+            else:
+                i+=1
+
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                result.append(i+1)
         return result
