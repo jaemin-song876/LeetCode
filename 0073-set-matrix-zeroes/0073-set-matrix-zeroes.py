9b6c7b25f41e9f6dfcf557597f06b15fe86e1a16
Jaemin Song <139603039+jaemin-song876@users.noreply.github.com>
2025-07-06 22:47:04 -0500

    Time: 8 ms (33.07%), Space: 13.1 MB (72.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,19 +4,28 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        zero_in_First_col = False
-        for row in range(len(matrix)):
-            if matrix[row][0] == 0:
-                zero_in_First_col =  True
-            
-            for col in range(1, len(matrix[0])):
-                if matrix[row][col] == 0:
-                    matrix[row][0] = 0
-                    matrix[0][col]=0
+
+        rows, cols = len(matrix), len(matrix[0])
+        first_row_has_zero = any(matrix[0][j] == 0 for j in range(cols))
+        first_col_has_zero = any(matrix[i][0] == 0 for i in range(rows))
+
+        #use the first row and first column to mark zero positions
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = matrix[0][j] = 0
         
-        for row in range(len(matrix)-1, -1, -1):
-            for col in range(len(matrix[0])-1, 0, -1):
-                if matrix[row][0] == 0 or matrix[0][col] == 0:
-                    matrix[row][col] = 0
-            if zero_in_First_col:
-                    matrix[row][0] =0
+        #set elements to zero based on the first row and first column marks
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][0] == 0 or matrix[0][j] ==0 :
+                    matrix[i][j] = 0
+
+        #set first row and first column to zero if they were marked
+        if first_row_has_zero:
+            for j in range(cols):
+                matrix[0][j] = 0
+        
+        if first_col_has_zero:
+            for i in range(rows):
+                matrix[i][0] = 0
