4e3ca2c69633b03aed9ff208206e97a40511d0e2
Jaemin Song <139603039+jaemin-song876@users.noreply.github.com>
2025-07-16 09:55:14 -0500

    Time: 218 ms (78.44%), Space: 27.7 MB (89.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,26 +4,33 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        island = 0
+        rows, cols = len(grid), len(grid[0])
+        island =0
 
-        rows = len(grid)
-        cols = len(grid[0])
+        def bfs(start_row, start_col):
+            queue = deque()
+            queue.append((start_row, start_col))
+            grid[start_row][start_col]='0'
 
-        def dfs(r,c):
-            if not (0 <= r < rows and 0 <= c <cols): return
-            if grid[r][c] == '0':
-                return
-            grid[r][c] = '0'
-
-            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-            for dr, dc in directions:
-                dfs(r+dr, c+dc)
-
-            
+            directions =[(-1,0),(1,0),(0,-1),(0,1)]
+            while queue:
+                row, col = queue.popleft()
+                for d_row, d_col in directions:
+                    new_row = row + d_row
+                    new_col = col+d_col
+            #유효하고 땅이면
+                    if (
+                        0 <= new_row <rows and
+                        0 <= new_col <cols and 
+                        grid[new_row][new_col] == '1'
+                    ):
+                        queue.append((new_row, new_col))
+                        grid[new_row][new_col] = '0'
+            #전체 grid 돌면서 bfs 시작 지점 찾기
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] =='1':
+                if grid[r][c] == '1':
                     island += 1
-                    dfs(r, c)
+                    bfs(r,c)
+                
         return island
