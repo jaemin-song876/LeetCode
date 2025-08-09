6887858c96946f9674e6b6e0d6fef7040cc700a5
Jaemin Song <139603039+jaemin-song876@users.noreply.github.com>
2025-08-09 17:26:48 -0500

    Time: 0 ms (100%), Space: 12.5 MB (43.21%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,29 +6,16 @@
 #         self.right = right
 class Solution(object):
     def binaryTreePaths(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[str]
-        """
-
-        result_route=[]
-        def dfs(node, current_path):
-
-            if not node:
-                return []
-
+        res = []
+        if not root:
+            return res
+        stack = [(root, str(root.val))]
+        while stack:
+            node, path = stack.pop()
             if not node.left and not node.right:
-                result_route.append(current_path)
-                return
-
-            #왼쪽 자식 노드가 있으면 탐색
-            if node.left:
-                dfs(node.left, current_path + "->"+ str(node.left.val))
-
+                res.append(path)
             if node.right:
-                dfs(node.right, current_path + "->"+ str(node.right.val))
-
-        if root:
-            dfs(root, str(root.val))
-
-        return result_route
+                stack.append((node.right, path + "->" + str(node.right.val)))
+            if node.left:
+                stack.append((node.left, path + "->" + str(node.left.val)))
+        return res
